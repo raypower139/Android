bc744d5b1d300f7e14c27325338199075747ef7c
Ray Power <39620873+raypower139@users.noreply.github.com>
2019-10-20 23:04:02 +0100

    List of Placemarks Added

---
+++ app/src/main/java/org/wit/placemark/activities/PlacemarkActivity.kt
@@ -0,0 +1,41 @@
+package org.wit.placemark.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_placemark.*
+import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.info
+import org.jetbrains.anko.toast
+import org.wit.placemark.R
+import org.wit.placemark.main.MainApp
+import org.wit.placemark.models.PlacemarkModel
+
+class PlacemarkActivity : AppCompatActivity(), AnkoLogger {
+
+  var placemark = PlacemarkModel()
+  lateinit var app : MainApp
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_placemark)
+    info("Placemark Activity started..")
+
+    app = application as MainApp
+
+    btnAdd.setOnClickListener() {
+      placemark.title = placemarkTitle.text.toString()
+      placemark.description = description.text.toString()
+      if (placemark.title.isNotEmpty()) {
+        app.placemarks.add(placemark.copy())
+        info("add Button Pressed: $placemarkTitle")
+        app.placemarks.forEach { info("add Button Pressed: ${it}")}
+        setResult(AppCompatActivity.RESULT_OK)
+        finish()
+      }
+      else {
+        toast ("Please Enter a title")
+      }
+    }
+  }
+}
+
